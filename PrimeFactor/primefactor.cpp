c72936845c2cb7f65157d24d643036b4f5f4618d
joenggyun.yu <jeonggyun@gmail.com>
2024-07-03 11:26:53 +0900

    Page 59

--- PrimeFactor/primefactor.cpp
+++ PrimeFactor/primefactor.cpp
@@ -7,20 +7,12 @@ public:
 	vector<int> of(int number)
 	{
 		vector<int> result = {};
-		if (number > 1) {
-			int devisor = 2;
-			if (number == 4|| number == 6 || number == 9 || number == 12) {
-				for (devisor = 2; number > 1; devisor++)
-				{
-					while (number % devisor == 0)
-					{
-						result.push_back(devisor);
-						number /= devisor;
-					}
-				}
-			}
-			else {
-				result.push_back(number);
+		for (int devisor = 2; number > 1; devisor++)
+		{
+			while (number % devisor == 0)
+			{
+				result.push_back(devisor);
+				number /= devisor;
 			}
 		}
 
