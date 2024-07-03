15808d4521af54d7bf80d06f83045d0ef5fc4853
joenggyun.yu <jeonggyun@gmail.com>
2024-07-03 11:21:26 +0900

    Page 52

--- PrimeFactor/primefactor.cpp
+++ PrimeFactor/primefactor.cpp
@@ -8,11 +8,22 @@ public:
 	{
 		vector<int> result = {};
 		if (number > 1) {
+			int devisor = 2;
 			if (number == 4) {
-				while (number % 2 == 0)
+				while (number % devisor == 0)
 				{
-					result.push_back(2);
-					number /= 2;
+					result.push_back(devisor);
+					number /= devisor;
+				}
+			}
+			else if (number == 6) {
+				for (devisor = 2; number > 1; devisor++)
+				{
+					while (number % devisor == 0)
+					{
+						result.push_back(devisor);
+						number /= devisor;
+					}
 				}
 			}
 			else {
