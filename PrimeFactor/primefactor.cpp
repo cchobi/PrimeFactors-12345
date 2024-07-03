48fcd479db03d1181fcbac937988bad2f8b72e3a
joenggyun.yu <jeonggyun@gmail.com>
2024-07-03 10:58:03 +0900

    page 34

--- PrimeFactor/primefactor.cpp
+++ PrimeFactor/primefactor.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class PrimeFactor {
 public:
-
+	vector<int> of1(int i)
+	{
+		vector<int> result = {};
+		return result;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
-TEST(PrimeFactors, PrimeTest) {
-  EXPECT_EQ(1, 1);
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of1(1));
 }
